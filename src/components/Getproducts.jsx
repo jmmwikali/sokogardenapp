77b32ac03b7cf5614a05e4303569ce257100d51f
Jessica <jmmwikali936@gmail.com>
2026-03-17 14:27:12 +0300

    get products component

--- src/components/Getproducts.jsx
+++ src/components/Getproducts.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import Loader from './Loader';
 
 const Getproducts = () => {
+
+  // Initialize hooks to help you manage the state of your application
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  // Below we specify the image base URL
+  const img_url = "https://jmmwikali.alwaysdata.net/static/images/" 
+
+  // Create a function to help you fetch the products from your API
+  const fetchProducts = async() => {
+    try{
+
+      // Update the loading hook.
+      setLoading(true);
+
+      // Interact with your endpoint for fetching the products.
+      const response = await axios.get("https://jmmwikali.alwaysdata.net/api/get_products")
+
+      // Update the products hook with the response given from the api
+      setProducts(response.data);
+
+      // Set the loading hook back to default
+      setLoading(false);
+    }
+    catch(error){
+      // If there is an error:
+      // Set loading hook back to default
+      setLoading(false)
+
+      // Update the error hook with a message
+      setError(error.message)
+    }
+  }
+
+  // We shall use the useEffect hook. This hook enables us to outomatically re-render new features incase of any changes
+  useEffect(() => {
+    fetchProducts()
+  }, []);
+
+  // console.log(products)
+
+
   return (
-    <div>
-      <h1>Welcome to the Get Products page</h1>
+    <div className='row'>
+      <h3 className="text-primary">Availabe Products</h3>
+
+      {loading && <Loader /> }
+      <h4 className="text-danger">{error}</h4>
+
+      {/* Map the products fetched from the api to the user interface */}
+
+      {products.map((product) => (
+        <div className="col-md-3 justify-content-center mb-3">
+          <div className="card shadow">
+            <img 
+            src={img_url + product.product_photo} 
+            alt="product name"
+            className='product_img mt-2 p-2' />
+
+            <div className="card-body">
+              <h5 className="text-primary">{product.product_name}</h5>
+
+              <p className="text-dark">{product.product_description.slice(0,20)}...</p>
+
+              <h4 className="text-warning">KES {product.product_cost} </h4>
+            </div>
+          </div>
+        </div>
+      ) )}
+
     </div>
   )
 }
